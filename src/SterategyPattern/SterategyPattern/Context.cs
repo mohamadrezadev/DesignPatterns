e1070dd6d6c2f54378ce78e1e0144c4be468efaf
mohamadrezadev <mohamadrezakiani8565@gmail.com>
2023-10-01 14:00:18 +0330

    add  SterategyPattern

---
+++ src/SterategyPattern/SterategyPattern/Context.cs
@@ -0,0 +1,14 @@
+﻿public class Context
+{
+    private Sterategy strategy;
+
+    public Context(Sterategy sterategy)
+    {
+        this.strategy = sterategy;
+    }
+
+    public void ContextInterface()
+    {
+        strategy.AlgorithmInterface();
+    }
+}
---
+++ src/SterategyPattern/SterategyPattern/Program.cs
@@ -0,0 +1,12 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+
+Context context;
+    context=new (new ConcreateSterategyA());
+context.ContextInterface();
+
+context = new(new ConcreateSterategyB());
+context.ContextInterface();
+
+ context = new(new ConcreateSterategyC());
+context.ContextInterface();
